a01119fca7195c14f5f2450ae0de037390853050
Diana Palafox <dianapalafox95@gmail.com>
2022-12-06 14:31:21 -0600

    Add initial layout

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,5 +1,23 @@
-
 import './App.css';
+import React from "react";
+import Education from "./components/Education"
+import Information from "./components/Information"
+import Experience from "./components/Experience"
+import Header from "./components/Header"
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Information />
+        <Education />
+        <Experience />
+      </div>
+    )
+  }
+}
 
 export default App;
+
+
